d7f75c3e1570c9d8da9867d2190b491a53110dd7
phamcuong8868 <phamcuong8868@gmail.com>
2026-05-02 15:53:12 +0700

    upgrade to version 3.0 favorite button

--- index_files/js/button.js
+++ index_files/js/button.js
@@ -1,7 +1,9 @@
 class MenuButton
 {
-    constructor (link,text,imageSrc,imageWidth)
+    constructor (pannelStt , id, link,text,imageSrc,imageWidth)
     {
+        this.pannelStt = pannelStt;
+        this.id = id;
         this.link = link;
         this.text = text;
         this.imageSrc = imageSrc;
@@ -30,8 +32,49 @@ class MenuButton
         }
         divv.appendChild(document.createTextNode(this.text));
         aa.appendChild(divv);
+
+        aa.addEventListener('click', (e) => {
+            e.preventDefault(); // chặn nhảy link ngay
+            this.increaseCount();
+            // mở link trong tab mới sau khi lưu
+            window.open(this.link, '_blank');
+        });
+
         return aa;
     }
     
+    increaseCount()
+    {        
+        let countKey = 'pvcMenuClick_count';
+        let countMap = JSON.parse(localStorage.getItem(countKey))  || {};
+        countMap[this.id] = (countMap[this.id] || 0) + 1;
+        localStorage.setItem(countKey, JSON.stringify(countMap));
+        renderFavoriteButtons();
+    }
+}
+
+function renderFavoriteButtons()
+{
+    favoriteMenu = document.getElementById('favoriteMenu');
+    let countKey = 'pvcMenuClick_count';
+    let countMap = JSON.parse(localStorage.getItem(countKey)) || {};
     
+    // Chuyển object thành mảng [id, count] và sắp xếp theo count giảm dần
+    const sortedEntries = Object.entries(countMap).sort((a, b) => b[1] - a[1]);
+
+    // Lấy 6 phần tử đầu tiên
+    const top6 = sortedEntries.slice(0, 6);
+    // Log ra 6 key có value lớn nhất
+    console.log('Top 6 keys with highest values:', top6.map(([id, count]) => id));
+
+    favoriteMenu.innerHTML = ''; // Xóa hết button cũ
+
+    // Render button mới theo thứ tự top 6
+    top6.forEach(([id, count]) => {
+        const menuItem = DSButton.find(item => item.id === id);
+        if (menuItem) {
+            const btn = new MenuButton(menuItem.pannelStt, menuItem.id, menuItem.link, menuItem.text, menuItem.imageSrc, menuItem.imageWidth);
+            favoriteMenu.appendChild(btn.toElement());
+        }
+    });
 }
