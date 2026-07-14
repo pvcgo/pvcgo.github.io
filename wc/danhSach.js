d5a48bed0eb86e5195b4118db19bdae65d71f01f
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2026-07-15 04:23:43 +0700

    Update danhSach.js

--- wc/danhSach.js
+++ wc/danhSach.js
@@ -46,16 +46,16 @@ const vong32 = new VongDau("Vòng 32 đội", [
           ]);
 
           const banKet = new VongDau("Bán kết", [
-              new TranDau(29, "15/07 02:00", "FRA", 0, "TayBanNha", 1),
+              new TranDau(29, "15/07 02:00", "FRA", 0, "TayBanNha", 2),
               new TranDau(30, "16/07 02:00", "ENG", null, "AcHen", null)
           ]);
 
           const tranhHangBa = new VongDau("Tranh hạng ba", [
-              new TranDau(31, "19/07 04:00", null, null, null, null)
+              new TranDau(31, "19/07 04:00", "FRA", null, null, null)
           ]);
 
           const chungKet = new VongDau("Chung kết", [
-              new TranDau(32, "20/07 02:00", null, null, null, null)
+              new TranDau(32, "20/07 02:00", "TayBanNha", null, null, null)
           ]);
 
           const danhSachVongDau = [vong32, vong16, tuKet, banKet, tranhHangBa, chungKet];
